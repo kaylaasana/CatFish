bcb02c3f2d1c50e46b629001be29eacfd9f33184
kayla <you@example.com>
2023-07-14 13:49:27 -0700

    pseuodo code for main game html and javascript

---
+++ assets/JS/script.js
@@ -0,0 +1,49 @@
+// element selectors
+    // character container
+    // container for commerce
+    // all the buttons (6 buttons)
+
+// global variables
+    // request URL
+    // money
+    // inventory
+
+// functions
+    // cast rod function
+        // fetch request
+        // fish name
+        // fish price
+        // image
+        // icon
+
+    // set interval timer for fish bite
+
+    // reel fish function
+
+    // set interval timer for reeling in fish
+
+    // get inventory function
+
+    // store fish function
+
+    // display success
+
+    // display failure
+
+    // change char img
+
+    // sell fish function
+
+    // display store
+   
+    // get random cat fact
+
+    // get random number(max, min)      
+
+// event listeners
+    // cast rod button
+    // reel in button
+    // backpack button
+    // store button
+    // sell fish button
+    // cat fact button
