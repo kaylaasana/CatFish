833b01b1b32bdabe13e0ce9d5c9328a05dae7280
moonjunsain <moonjunsain@gmail.com>
2023-07-14 16:49:33 -0700

    Homepage buttons and html created (modal)

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -1,7 +1,10 @@
 // element selectors
-    // start button
+// start button
+var startBtn = document.querySelector("#start-btn");
     // how to play button
-
+var how2PlayBtn = document.querySelector("#how-to-play-btn");
+var theModal = document.querySelector("#modal-how-to-play");
+var closingBtns = document.querySelectorAll(".closeModal");
 
 // function to start game
     // direct you to game page
@@ -15,3 +18,13 @@
 
 // event listener for start button
 // event listener for how to play
+
+how2PlayBtn.addEventListener("click", function(){
+    theModal.classList.add("is-active");
+})
+
+for(var i = 0; i < closingBtns.length; i++){
+    closingBtns[i].addEventListener("click", function(){
+        theModal.classList.remove("is-active");
+    })
+}
