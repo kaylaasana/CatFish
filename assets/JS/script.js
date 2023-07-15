43fcb43e514674e713a2daf973d3840c1726b09c
kayla <you@example.com>
2023-07-14 19:07:46 -0700

    javascript: element selectors, event lisenters and get inventory function. HTML: backpack inventory modal layout

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,9 @@
     // character container
     // container for commerce
     // all the buttons (6 buttons)
+var backpackBtn = document.querySelector('#closed');
+var backpackModal = document.querySelector('#backpack-modal');
+var exitBackpackBtn = document.querySelector('#exit-backpack');
 
 // global variables
     // request URL
@@ -17,18 +20,27 @@
         // icon
 
     // set interval timer for fish bite
+        
 
     // reel fish function
 
     // set interval timer for reeling in fish
+        
 
-    // get inventory function
+ function getInventory(){
+    backpackModal.classList.add("is-active");
+    
+ }
+        // kayla 
+        // open inventory - display
 
     // store fish function
 
     // display success
+        // minhee
 
     // display failure
+        // minhee
 
     // change char img
 
@@ -37,13 +49,19 @@
     // display store
    
     // get random cat fact
+        // june
 
-    // get random number(max, min)      
+    // get random number(max, min)    
+        // june  
 
 // event listeners
     // cast rod button
     // reel in button
     // backpack button
+backpackBtn.addEventListener("click", getInventory);
+exitBackpackBtn.addEventListener('click', function(){
+    backpackModal.classList.remove("is-active");
+})
     // store button
     // sell fish button
     // cat fact button
