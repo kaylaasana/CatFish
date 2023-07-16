0959c90c0531adeb7cec8afaec876e4f053a2d36
mchung03 <minhee.chung14@gmail.com>
2023-07-15 17:53:31 -0700

    how to play modal complete

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -87,43 +87,44 @@ var inventory = {
 
     // set interval timer for reeling in fish
         
-// update inventory
- function addToInventory(){
-    // create table row element
-    var inventoryRow = document.createElement('tr'); 
-    // create table data elements for each data point for that inventory item and append to table row
-    var fishIconRow = document.createElement('td');
-    fishIconRow.textContent = inventory.fishIcon
-    inventoryRow.append(fishIconRow);
+    // update inventory
+    function addToInventory(){
+        // create table row element
+        var inventoryRow = document.createElement('tr'); 
+        // create table data elements for each data point for that inventory item and append to table row
+        var fishIconRow = document.createElement('td');
+        fishIconRow.textContent = inventory.fishIcon
+        inventoryRow.append(fishIconRow);
+        
+        var fishNameRow = document.createElement('td');
+        fishNameRow.textContent = inventory.fishName
+        inventoryRow.append(fishNameRow);
+        
+        var fishPriceRow = document.createElement('td');
+        fishPriceRow.textContent = inventory.fishPrice
+        inventoryRow.append(fishPriceRow);
+        // need to create sell button for each line of inventory
+        var sellBtns = document.createElement('button')
+        sellBtns.setAttribute('class', 'sell-btns');
+        sellBtns,textContent = "Sell Fish";
+        inventoryRow.append(sellBtns);
+        // append data to table row
+        inventoryBody.append(inventoryRow);
+        // event listener for all sell buttons
+        sellBtns.addEventListener('click', function(){
+            // clear table row data
+            inventoryRow.remove();
+            // get current wallet price
+            // update wallet total
+            wallet.textContent = parseInt(wallet.textContent) + inventory.fishPrice;
+        })
+    };
     
-    var fishNameRow = document.createElement('td');
-    fishNameRow.textContent = inventory.fishName
-    inventoryRow.append(fishNameRow);
-    
-    var fishPriceRow = document.createElement('td');
-    fishPriceRow.textContent = inventory.fishPrice
-    inventoryRow.append(fishPriceRow);
-    // need to create sell button for each line of inventory
-    var sellBtns = document.createElement('button')
-    sellBtns.setAttribute('class', 'sell-btns');
-    sellBtns,textContent = "Sell Fish";
-    inventoryRow.append(sellBtns);
-    // append data to table row
-    inventoryBody.append(inventoryRow);
-    // event listener for all sell buttons
-    sellBtns.addEventListener('click', function(){
-        // clear table row data
-        inventoryRow.remove();
-        // get current wallet price
-        // update wallet total
-        wallet.textContent = parseInt(wallet.textContent) + inventory.fishPrice;
-    })
- };
-// display inventory
-function displayInventory(){
-    // display backpack modal
-    backpackModal.classList.add("is-active");
-}
+    // display inventory
+    function displayInventory(){
+        // display backpack modal
+        backpackModal.classList.add("is-active");
+    }
     // store fish function
 
     // display success
@@ -187,18 +188,16 @@ function displayInventory(){
         var randomNum = Math.floor(Math.random() * (max - min + 1) ) + min;
         return randomNum;
     }
-          
 
-
-// event listeners
-    // cast rod button
-    // reel in button
-    // backpack button
-backpackBtn.addEventListener("click", displayInventory);
-    // close backpack button
-exitBackpackBtn.addEventListener('click', function(){
-    backpackModal.classList.remove("is-active");
-});
+    // event listeners
+        // cast rod button
+        // reel in button
+        // backpack button
+    backpackBtn.addEventListener("click", displayInventory);
+        // close backpack button
+    exitBackpackBtn.addEventListener('click', function(){
+        backpackModal.classList.remove("is-active");
+    });
     // store button
     // sell fish button
     // cat fact button
